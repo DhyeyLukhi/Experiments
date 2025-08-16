7f5ab9e45a1958f2e42d8d9aecd58beb77e246dc
DhyeyLukhi <dhyeylukhi95@gmail.com>
2025-08-16 11:50:06 +0530

    Testing file changed for steganography

--- Tests 6 .py
+++ Tests 6 .py
@@ -1,16 +1,16 @@
-import keyboard
-import time
+string = "here is the string"
+binaryMessage = []
+for char in string:
+    ascii = ord(char)  # Get the ASCII value of the string
+    binary8 = format(ascii, '08b')  #Get the 8 bit value of string from the ASCII value
+    # Format function formats the string into 8 bits from 7 bits
 
-def recordKeys():
-    while True:
-        keys = keyboard.read_key()
-        event = keyboard.is_pressed(hotkey=keys)
-        if event:
-            print(f"Yes Checked {keys}")
-        
+    binaryMessage.append(binary8)
+print(binaryMessage)
 
-        
-       
+mybinary = ['01110011', '01111011', '01010001', '01010011', '01111111', '01110000']
 
-if __name__ == "__main__":
-    recordKeys()
+for pos in range(0, len(binaryMessage)):
+    for chars in range(0, 8):
+        if binaryMessage[pos][chars] == '1':
+            pass
